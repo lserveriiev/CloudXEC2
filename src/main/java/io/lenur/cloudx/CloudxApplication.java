cae44473ec01a1909245e0ba88658721bad04181
Lenur Serveriiev <lenur.serveriiev@spglobal.com>
2021-06-09 08:23:49 +0300

    added spring boot

---
+++ src/main/java/io/lenur/cloudx/CloudxApplication.java
@@ -0,0 +1,13 @@
+package io.lenur.cloudx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lenur/cloudx/CloudxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lenur.cloudx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
